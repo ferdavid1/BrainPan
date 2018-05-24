bfddda92f0400b1b0742f16d0695bdb0862ca437
ferdavid1 <fespinosa@ucdavis.edu>
2018-05-24 16:00:03 -0700

    small

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 import numpy as np 
 from pydub import AudioSegment
 from pydub.playback import play
+from scipy.io.wavfile import read, write
 
 def recognize_speech(audiofile):
 	from vad import VoiceActivityDetector
 	v = VoiceActivityDetector(audiofile)
 	detected = v.detect_speech()
-	return detected # returns array of window numbers and speech flags
+	# detected2 = v.convert_windows_to_readible_labels(detected)
+	return detected # returns array of window numbers and speech flags (1 - speech, 0 - nonspeech)
 
 def recognize_melody(audiofile):
 	pass
 
+def main(audiofile):
+	rate, signal = read(audiofile)
+	recognized = recognize_speech(audiofile)
+	inds = []
+	for ind, x in enumerate(recognized[:-1]):
+		for y in np.arange(int(x[0]), int(recognized[ind+1][0])):
+			inds.append(y)
+	print(len(inds))
+	speech = np.array([signal[ind] for ind in inds])
+	write('middleman.wav', 48100, speech)
+	# middlerate, middlesignal = read('middleman.wav')
 
-# song = AudioSegment.from_mp3("song.mp3")
-print(recognize_speech("song.mp3"))
-# pan the sound 15% to the right
-# panned_right = song.pan(+0.15)
+	song = AudioSegment.from_wav('middleman.wav')
+	# print(len(speech), len(recognized)) # (1580, 27409)
 
-# pan the sound 50% to the left
-# panned_left = song.pan(-0.50)
+	# pan the sound 15% to the right
+	panned_right = song.pan(+0.15)
 
-# #Play panned left audio
-# while True:
-#     try:
-#         play(panned_left)
-#     except KeyboardInterrupt:
-#         print "Stopping playing"
-#         break
+	# pan the sound 50% to the left
+	# panned_left = song.pan(-0.50)
+
+	# #Play panned left audio
+	from pydub.playback import play
+	# play(panned_left)
+
+if __name__ == '__main__':
+	main("song.wav")
